5937f83606114d9ec1b97b15e7f650d37a59c1ea
opeyemi <elubanjoo@gmail.com>
2021-07-12 03:45:35 -0700

    Tosin change

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let display = document.getElementById('display');
-let buttons = Array.from(document.getElementsByClassName('button'))
+let buttons = Array.from(document.getElementsByClassName('button'));
 
 buttons.map((button, index) => {
     button.addEventListener('click', (e) => {
