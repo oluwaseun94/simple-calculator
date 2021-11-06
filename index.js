2aed3fcec38ea32eda0ec7735436cae9e73251bd
opeyemi <84335502+oluwaseun94@users.noreply.github.com>
2021-11-06 16:51:03 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map((button, index) => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText) {
+            case 'DEL' :
+              if(display.innerText) {
+                display.innerText = display.innerText.slice(0, -1)
+              }
+              break;
+            case '=' : 
+              if(display.innerText) {
+                display.innerText = eval(display.innerText);
+              }
+              break;
+              case '√' :
+              if(display.innerText) {
+                display.innerText = Math.sqrt(display.innerText)
+              }
+              break;
+
+            default :
+              display.innerText += e.target.innerText;
+        }
+    })
+})
